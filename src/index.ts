f6bde3c0ea356d8316f44c2b51c7a4a78b73ed42
Nas Fong <fongren007@gmail.com>
2023-02-23 08:50:55 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const PORT = process.env.PORT || 3004
+
+const app: Application = express()
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+})
+
+
+
+
+
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hello my friend')
+  console.log('hello')
+
+})
+
+httpServer.listen(PORT, () => console.log(`Welcome http://localhost:${PORT}`))
